a866d79e6ef62daccc90da20651ef9bae61a37c9
visiontechhhh360-lgtm <visiontechhhh360@gmail.com>
2026-07-03 13:53:05 +0500

    Rename privacy route to privacy-policy

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ const App = () => (
           <Route path="/download" element={<Download />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/privacy-policy" element={<Privacy />} />
           <Route path="/terms" element={<Terms />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
